389c1e6fa84c00aa6a7a3a170042808bc3e1c11f
Nick Weaver <nicholas.j.weaver@gmail.com>
2019-08-27 22:37:53 -0400

    2nd push

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,12 +1,13 @@
 // @TODO: YOUR CODE HERE!
-var svgWidth = 500;
+function makeResponsive(){
+var svgWidth = 1000;
 var svgHeight = 500;
 
 var chartMargin = {
-    top:30,
-    right: 30,
-    bottom: 30,
-    left: 30
+    top:70,
+    right: 70,
+    bottom: 70,
+    left: 70
 }
 
 var chartWidth = svgWidth - chartMargin.left -chartMargin.right;
@@ -19,11 +20,10 @@ var svg = d3.select("#scatter")
 var chartGroup = svg.append("g")
                     .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
-d3.csv("../assets/data/data.csv", function(error, data){
-    if (error) throw error;
 
-    // console.log(data);
-    data.forEach(function(d){
+d3.csv('assets/data/data.csv').then(function(stateData){
+    console.log(stateData);
+    stateData.forEach(function(d){
         d.poverty = +d.poverty;
         d.povertyMoe= +d.povertyMoe;
         d.age= +d.age;
@@ -33,14 +33,14 @@ d3.csv("../assets/data/data.csv", function(error, data){
         d.healthcare= +d.healthcare;
         d.healthcareLow= +d.healthcareLow;
         d.healthcareHigh = +d.healthcareHigh;
-    })
+    });
 
     var yScale = d3.scaleLinear()
-                    .domain([0, d3.max(data.obesity)])
+                    .domain([d3.max(stateData, d=>d.obesity),16])
                     .range([0,chartHeight]);
 
-    var xScale = d3.scaleBand()
-                    .domain([data.abbr])
+    var xScale = d3.scaleLinear()
+                    .domain([8.5, d3.max(stateData, d=>d.poverty)])
                     .range([0,chartWidth]);
 
     var yAxis = d3.axisLeft(yScale);
@@ -52,5 +52,38 @@ d3.csv("../assets/data/data.csv", function(error, data){
     chartGroup.append("g")
                 .call(yAxis);
 
+    chartGroup.append("text")             
+                .attr("transform", `translate(${chartWidth/2}, ${chartHeight+32})`)
+                .style("text-anchor", "middle")
+                .text("% in Poverty");
 
+    chartGroup.append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 0 - chartMargin.left)
+                .attr("x",0 - (chartHeight / 2))
+                .attr("dy", "1em")
+                .style("text-anchor", "middle")
+                .text("Obesity %");              
+
+    var circlesGroup = chartGroup.selectAll("circle")
+                .data(stateData)
+                .enter()
+                .append("circle")
+                .attr("cx", d => xScale(d.poverty))
+                .attr("cy", d => yScale(d.obesity))
+                .attr("r", "15")
+                .attr("fill", "blue")
+                .attr("opacity", ".5")
+ 
+   
+    var textGroup = chartGroup.selectAll("text")
+               .data(stateData)
+               .enter()
+               .append("text")
+               .attr("x", d => (xScale(d.poverty)-11))
+               .attr("y", d => (yScale(d.obesity)+7))
+               .text(d=>d.abbr)
+               .attr("fill", "white");
 });
+};
+makeResponsive();
